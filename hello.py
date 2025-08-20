2c5d61a70d02af6876212602e98becf315b40403
revian <nassim.coder@proton.me>
2025-08-20 13:14:12 +0100

    adding some stuff on the hello file

--- hello.py
+++ hello.py
@@ -1,1 +1,6 @@
 print("Hellow motherfucking world")
+# This no longer a hello world program
+#let's add some stuff man this will be cool
+
+print("This is the second line of the program")
+print("And this is the third one")
